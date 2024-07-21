9510f83a141c6b0c8a8b7ad37d4a349b94fb11b0
Vishwa Udayanga <udhayangavishwa@gmail.com>
2024-07-21 18:33:30 +0530

    text center

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         />
         <h1 className="text-3xl font-bold mt-2">Comic Wave - <span className="text-violet-600">2024</span></h1>
         <p className="text-sm mt-2 text-center text-slate-400 text-wrap break-words">Combines an E-sport gathering and an on premise Comic-Con <br />Grab your tickets for an elegant day that will be full of joy, love, and friendship.</p>
-        <Link href={'/'} className="pl-16 pr-16 pb-3 pt-3 custom-purple-low text-xs rounded mt-5">Buy Tickets Right Now</Link>
+        <Link href={'/'} className="pl-16 pr-16 pb-3 pt-3 custom-purple-low text-xs rounded mt-5 text-center">Buy Tickets Right Now</Link>
         <Image 
           src={'/main/main-img.png'}
           alt={'Main | Image'}
