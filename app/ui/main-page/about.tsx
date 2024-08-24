dd43243d54043a54cda3eb938916cdccb3f066d5
Vishwa Udayanga <udhayangavishwa@gmail.com>
2024-08-24 12:00:47 +0530

    Ticket info updated2

--- app/ui/main-page/about.tsx
+++ app/ui/main-page/about.tsx
@@ -1,4 +1,5 @@
 export default function About() {
+    const whatsappUrl = `https://wa.me/+94772522355`;
     return (
         <section className="flex pt-20 pr-4 pl-4 pb-20 justify-center items-center custom-dark-high flex-col" style={{backgroundImage : "url('/main/about1.jpg')", backgroundSize: 'cover'}}>
                 <div className="flex w-full justify-center md:w-2/4 flex-col items-center">
@@ -9,7 +10,13 @@ export default function About() {
                         href="mailto:comic.wave.event@gmail.com?subject=Contact%20ComicWave"
                         className="pl-16 pr-16 pb-3 pt-3 custom-purple-low text-xs rounded mt-5 text-center inline-block"
                         >
-                        Contact ComicWave
+                        Contact ComicWave Via Email
+                    </a>
+                    <a
+                        href={whatsappUrl} target="_blank" rel="noopener noreferrer"
+                        className="pl-16 pr-16 pb-3 pt-3 custom-purple-low text-xs rounded mt-5 text-center inline-block"
+                        >
+                        Contact ComicWave Via Whatsapp
                     </a>
                 </div>
         </section>
